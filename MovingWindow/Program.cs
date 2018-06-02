83da1c86eaa439fc93b5471da4f26b2d133e915b
Константин Курочкин <kostya45721@gmail.com>
2018-06-02 19:49:01 +0300

    Форматирование объявления типов

--- MovingWindow/Program.cs
+++ MovingWindow/Program.cs
@@ -3,7 +3,7 @@ using System.Windows.Forms;
 
 namespace MovingWindow
 {
-    static class Program
+    public static class Program
     {
         /// <summary>
         /// The main entry point for the application.
